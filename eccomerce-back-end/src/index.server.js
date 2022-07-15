2394523abd82f695c89cd46c5b066c4f5ec52ca7
Rishabhkumar1714 <rishabhkumar7148@gmail.com>
2022-07-15 06:12:42 +0530

    second commit

--- eccomerce-back-end/src/index.server.js
+++ eccomerce-back-end/src/index.server.js
@@ -7,6 +7,9 @@ const bodyParser =require('body-parser')
 //environment variable 
 env.config()
 
+//Routes
+const userRoutes=require('./routes/user')
+
 // mongodb connection
 
 const db=`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.csfkg.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
@@ -20,18 +23,20 @@ mongoose.connect(db).then(()=>{
 
 app.use(bodyParser())
 
+app.use('/api',userRoutes)
 
-app.get('/',(req,res)=>{
-    res.status(200).json({
-        message:'Hello from the Server'
-    })
-})
 
-app.post('/data',(req,res)=>{
-    res.status(200).json({
-        message:req.body
-    })
-})
+// app.get('/',(req,res)=>{
+//     res.status(200).json({
+//         message:'Hello from the Server'
+//     })
+// })
+
+// app.post('/data',(req,res)=>{
+//     res.status(200).json({
+//         message:req.body
+//     })
+// })
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT} `)
